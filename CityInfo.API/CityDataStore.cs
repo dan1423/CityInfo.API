4c89c985bab788896fb1d1b08376d81b78367b0c
dan1423 <oluwadare.daniel21@gmail.com>
2020-05-15 12:51:20 -0500

    initial commit

---
+++ CityInfo.API/CityDataStore.cs
@@ -0,0 +1,49 @@
+﻿using CityInfo.API.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API
+{
+    public class CityDataStore
+    {
+        public static CityDataStore Current { get; } = new CityDataStore();
+        public List<CityDto> Cities { get; set; }
+
+        public CityDataStore()
+        {
+            Cities = new List<CityDto>
+            {
+                new CityDto()
+                {
+                  Id=1,
+                  Name = "New York City",
+                  Description = "The city of cities",
+                  PointsOfInterest = new List<PointOfInterestDto>()
+                  {
+                      new PointOfInterestDto()
+                      {
+                           Id = 1,
+                          Name = "Central Park",
+                          Description ="Cool park"
+                      },
+                      new PointOfInterestDto()
+                      {
+                           Id = 2,
+                          Name = "Empire State Building",
+                          Description ="Cool skyscraper"
+                      }
+
+                  }
+                },
+                new CityDto
+                {
+                     Id=2,
+                  Name = "Chicago",
+                  Description = "Windy city"
+                }
+            };
+        }
+    }
+}
